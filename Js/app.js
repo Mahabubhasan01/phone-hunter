5390c11440c2f57c2a8d7707d73827481230ae88
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-03-01 11:57:38 +0600

    first api set ok

--- Js/app.js
+++ Js/app.js
@@ -1,16 +1,18 @@
  allBrand = () =>{
+     document.getElementById('card').innerHTML='';
      const searchText = document.getElementById('search');
      const searchValue = searchText.value;
-    const url = "https://openapi.programming-hero.com/api/phones?search='${searchValue}'";
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
     fetch(url)
     .then(res=>res.json())
     .then(data=>brandInfo(data.data))
     
  }
   const brandInfo = (datas) =>{
-    const parent = document.getElementById('card')
-    for(const data of datas){
-        console.log(data)
+    const parent = document.getElementById('card');
+    const sliceValue = datas.slice(0,20);
+/*     console.log(sliceValue)
+ */    for(const data of sliceValue){
         const div = document.createElement('div');
         div.classList.add('col-sm-1', 'col-md-4', 'col-lg-4')
         div.innerHTML=
@@ -20,7 +22,9 @@
         <div class="card-body">
         <h5 class="card-title">${data.phone_name}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        
+        <div class="d-flex"><a href="#" class="btn btn-primary">Explore</a>
+        <a href="#" class="btn btn-primary">Detail</a></div>
         </div>
     </div>
 </div>` 
